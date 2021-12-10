b0b5060d327e4467acdf8a90b44e55c35e3c0ee7
Tho <Tho@example.com>
2021-12-10 15:08:06 +0700

    add db file

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 	//khai bao cac bien ket noi csdl
-	$hostname = "remotemysql.com";
-	$user = "hl2yurXRxN";
-	$password = "DTM8rtmh2I";
-	$database = "hl2yurXRxN";
+	$hostname = "localhost";
+	$user = "root";
+	$password = "";
+	$database = "dynamicweblab";
  ?>
